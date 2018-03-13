205221a4a7751ba0b46bacb3621595d5d9225342
smartbooks <smartbooks@qq.com>
2018-03-13 15:55:26 +0800

    modified:   main/java/com/facebook/presto/plugin/oracle/OracleClient.java 	modified:   ../../pom.xml

--- presto-oracle/src/main/java/com/facebook/presto/plugin/oracle/OracleClient.java
+++ presto-oracle/src/main/java/com/facebook/presto/plugin/oracle/OracleClient.java
@@ -14,32 +14,31 @@
 package com.facebook.presto.plugin.oracle;
 
 import com.facebook.presto.plugin.jdbc.*;
+import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.SchemaTableName;
+import com.facebook.presto.spi.TableNotFoundException;
 import com.facebook.presto.spi.type.Type;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import io.airlift.log.Logger;
 import oracle.jdbc.OracleDriver;
 
 import javax.inject.Inject;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Properties;
-import java.util.Set;
+import java.sql.*;
+import java.util.*;
 
 import static com.facebook.presto.plugin.jdbc.DriverConnectionFactory.basicConnectionProperties;
+import static com.facebook.presto.plugin.jdbc.JdbcErrorCode.JDBC_ERROR;
 import static java.util.Locale.ENGLISH;
 
 public class OracleClient
-        extends BaseJdbcClient
-{
+        extends BaseJdbcClient {
     @Inject
-    public OracleClient(JdbcConnectorId connectorId, BaseJdbcConfig config, OracleConfig oracleConfig)
-    {
+    public OracleClient(JdbcConnectorId connectorId, BaseJdbcConfig config, OracleConfig oracleConfig) {
         super(connectorId, config, "", connectionFactory(config, oracleConfig));
     }
 
-    private static ConnectionFactory connectionFactory(BaseJdbcConfig config, OracleConfig oracleConfig)
-    {
+    private static ConnectionFactory connectionFactory(BaseJdbcConfig config, OracleConfig oracleConfig) {
         Properties connectionProperties = basicConnectionProperties(config);
 
         if (oracleConfig.isAutoReconnect()) {
@@ -54,41 +53,70 @@ public class OracleClient
     }
 
     @Override
-    public Set<String> getSchemaNames()
-    {
+    public Set<String> getSchemaNames() {
         try (Connection connection = connectionFactory.openConnection();
-                ResultSet resultSet = connection.getMetaData().getSchemas()) {
+             ResultSet resultSet = connection.getMetaData().getSchemas()) {
             ImmutableSet.Builder<String> schemaNames = ImmutableSet.builder();
             while (resultSet.next()) {
                 String schemaName = resultSet.getString(1).toLowerCase(ENGLISH);
                 schemaNames.add(schemaName);
             }
             return schemaNames.build();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
     protected ResultSet getTables(Connection connection, String schemaName, String tableName)
-            throws SQLException
-    {
-        return connection.getMetaData().getTables(connection.getCatalog(), schemaName, tableName, new String[] {"TABLE", "SYNONYM"});
+            throws SQLException {
+        return connection.getMetaData().getTables(connection.getCatalog(), schemaName, tableName, new String[]{"TABLE", "SYNONYM"});
     }
 
     @Override
     protected SchemaTableName getSchemaTableName(ResultSet resultSet)
-            throws SQLException
-    {
+            throws SQLException {
         return new SchemaTableName(
-                resultSet.getString("TABLE_CAT").toLowerCase(ENGLISH),
+                resultSet.getString("TABLE_SCHEM").toLowerCase(ENGLISH),
                 resultSet.getString("TABLE_NAME").toLowerCase(ENGLISH));
     }
 
     @Override
-    protected String toSqlType(Type type)
-    {
-        return super.toSqlType(type);
+    public List<JdbcColumnHandle> getColumns(JdbcTableHandle tableHandle) {
+        try (Connection connection = connectionFactory.openConnection()) {
+            try (ResultSet resultSet = getColumns(tableHandle, connection.getMetaData())) {
+                List<JdbcColumnHandle> columns = new ArrayList<>();
+                while (resultSet.next()) {
+                    JdbcTypeHandle typeHandle = new JdbcTypeHandle(
+                            resultSet.getInt("DATA_TYPE"),
+                            resultSet.getInt("COLUMN_SIZE"),
+                            resultSet.getInt("DECIMAL_DIGITS"));
+                    Optional<ReadMapping> columnMapping = toPrestoType(typeHandle);
+                    // skip unsupported column types
+                    if (columnMapping.isPresent()) {
+                        String columnName = resultSet.getString("COLUMN_NAME");
+                        columns.add(new JdbcColumnHandle(connectorId, columnName, typeHandle, columnMapping.get().getType()));
+                    }
+                }
+
+                if (columns.isEmpty()) {
+                    // In rare cases (e.g. PostgreSQL) a table might have no columns.
+                    throw new TableNotFoundException(tableHandle.getSchemaTableName());
+                }
+
+                return ImmutableList.copyOf(columns);
+            }
+        } catch (SQLException e) {
+            throw new PrestoException(JDBC_ERROR, e);
+        }
+    }
+
+    private ResultSet getColumns(JdbcTableHandle tableHandle, DatabaseMetaData metadata)
+            throws SQLException {
+        return metadata.getColumns(
+                tableHandle.getCatalogName(),
+                tableHandle.getSchemaName(),
+                tableHandle.getTableName(),
+                null);
     }
 }
